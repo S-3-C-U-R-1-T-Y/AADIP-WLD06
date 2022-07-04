b4239502d6539a482c93e3117e3a6e0f7a84f34c
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-07-04 09:18:50 +0200

    Create TestFlow.go

---
+++ TestFlow.go
@@ -0,0 +1,4 @@
+"AadClientId": "9c23c22a-7afd-4c9a-baa6-b614ec14074d",
+"AadSecret": "D1Y7Q~7xOWtITztU6tpp3Zhfv5ThBIvvOMpvB",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
